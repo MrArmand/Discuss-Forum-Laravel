3aaea02f705c61c6391d7541ed30e3a78cab7685
Armand Dorosz <armanddorosz@gmail.com>
2022-12-27 13:32:00 +0000

    Added home and fixed naming for posts

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return "This is /home page!";
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
