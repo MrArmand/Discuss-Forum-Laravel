c4e083808ea1245524e87d23a1e5d23cab31d627
Armand Dorosz <armanddorosz@gmail.com>
2023-01-03 18:45:40 +0000

    Different posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,26 +2,6 @@
 
 @section('content')
     
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
 </div>
 <div class="container">
   <nav class="breadcrumb">
@@ -34,11 +14,11 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __("Armand") }}</div>
+                <div class="card-header">{{ ($post->user) }}</div>
 
                 <div class="card-body">
                     
-                    {{ __("amogus") }}
+                    {{ __($post->content) }}
                     
                 </div>
             </div>
