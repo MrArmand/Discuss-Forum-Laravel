f499581d1de94ee0c809b67d05f400906dfc75cf
Armand Dorosz <armanddorosz@gmail.com>
2023-01-03 19:42:02 +0000

    working user_id with posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ ($post->user) }}</div>
-
+                <div class="card-header">{{ $post->user_id }}</div>
                 <div class="card-body">
                     
                     {{ __($post->content) }}
