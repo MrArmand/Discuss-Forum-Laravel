a571d54a8b46acdc71c8487981ffe9ffa4d80d70
Armand Dorosz <armanddorosz@gmail.com>
2023-01-03 18:30:00 +0000

    created posts boxes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,11 +22,29 @@
     </div>
 </div>
 
-<p>Dashboard</p>
-    <ul>
-        @foreach ($posts as $post)
-            <li>{{$post -> content}}</li>
-            
-        @endforeach
+</div>
+<div class="container">
+  <nav class="breadcrumb">
+    <a href="#" class="breadcrumb-item active"> Dashboard</a>
+    <a href="#" class="breadcrumb-item active"> News</a>
+  </nav>
 
+  @foreach ($posts as $post)
+  <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __("Armand") }}</div>
+
+                <div class="card-body">
+                    
+                    {{ __("amogus") }}
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>  
+@endforeach 
+       
 @endsection
