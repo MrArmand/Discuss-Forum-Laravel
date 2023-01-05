6e28e1a7d6f1cd5b163ab5f4c0c5296024bcc892
Armand Dorosz <armanddorosz@gmail.com>
2023-01-05 10:34:05 +0000

    tried fixing comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,11 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
     use HasFactory;
-
+    use SoftDeletes;
+   
+    protected $dates = ['deleted_at'];
+   
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'post_id', 'content'];
+   
+    /**
+     * The belongs to Relationship
+     *
+     * @var array
+     */
     public function users(){
         return $this->belongsTo(Post::class);
     }
