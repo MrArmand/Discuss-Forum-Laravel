2ae14e329a643184a666037a5f2d181bb7afb977
Armand Dorosz <armanddorosz@gmail.com>
2023-01-07 17:52:36 +0000

    finish

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,15 +1,14 @@
 @if ($post->comments != null)
-                                @foreach ($post->comments as $comment)
-                                    <nav class="">
-                                        <div class="container">
-                                            <div class="card">
-                                                    <div class="card-header"><a
-                                                            href="/user/{{ $comment->user_id }}">{{ $post->username($comment->user_id) }}</a>
-                                                            @include('comments.index', ['comment' => $comment])  
-                                                    </div>
-                                                    {{ $comment->content }}
-                                                                                            
-                                            </div>
-                                        </div>
-                                @endforeach
-                            @endif
+    @foreach ($post->comments as $comment)
+        <nav class="">
+            <div class="container">
+                <div class="card">
+                    <div class="card-header"><a
+                            href="/user/{{ $comment->user_id }}">{{ $post->username($comment->user_id) }}</a>
+                        @include('comments.index', ['comment' => $comment])
+                    </div>
+                    {{ $comment->content }}
+                </div>
+            </div>
+    @endforeach
+@endif
