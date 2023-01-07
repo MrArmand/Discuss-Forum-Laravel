105dd454ef4fccb5c433c1f7fcb3cd40c744cf02
Armand Dorosz <armanddorosz@gmail.com>
2023-01-07 08:58:16 +0000

    made pins (manytomany)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,44 +7,13 @@
         <a href="#">2</a>
         <a href="#">&raquo;</a>
     </div>
+
     @foreach ($posts as $post)
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col">
-                    <div class="card">
-                        <div class="card-header"><a
-                                href="/user/{{ $post->user_id }}">{{ $post->username($post->user_id) }}</a>
-                        </div>
-                        <div class="card-body">
+    @if (auth()->user()->id == $post->pin)
+        @include('posts.show', ['post' => $post])
+    @endforeach
 
-                            {{ __($post->content) }}
-
-                            @include('comments.show', ['post' => $post])
-
-                        </div>
-                        @include('posts.index', ['post' => $post])
-                    </div>
-
-
-
-
-                    <meta name="csrf-token" content="{{ csrf_token() }}">
-                    <nav class="breadcrumb">
-                        <div class="container">
-                            @if (session('status'))
-                                <div class="alert alert-success">
-                                    {{ session('status') }}
-                                </div>
-                            @endif
-
-                            @include('comments.create', ['post' => $post])
-
-                        </div>
-                </div>
-            </div>
-        </div>
-        </div>
-
-        </div>
+    @foreach ($posts as $post)
+        @include('posts.show', ['post' => $post])
     @endforeach
 @endsection
