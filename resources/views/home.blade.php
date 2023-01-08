28789f4e331dd7ca1a1bcfa59aa4fa45b5ddddcc
Armand Dorosz <armanddorosz@gmail.com>
2023-01-08 11:43:33 +0000

    applied edit comment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex-center pagination">
-        <a href="dashboard">&laquo;</a>
-        <a href="dashboard"class="active">1</a>
-        <a href="dashboard">2</a>
-        <a href="dashboard">&raquo;</a>
-    </div>
+
+    
 
     @if (auth()->user()->role == 1)
         <div class="content links">
-            <p><b>{{("YOU ARE LOGGED AS AN ADMIN.")}}</b></p>
+            <p><b>{{ 'YOU ARE LOGGED AS AN ADMIN.' }}</b></p>
         </div>
     @endif
 
